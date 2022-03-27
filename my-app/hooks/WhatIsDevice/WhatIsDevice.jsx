35efddf24dcf32276f6cff27da13e18530ab6292
Aleksandr Ermolov <AYErmolov@sberbank.ru>
2022-03-27 15:49:17 +0300

    hoock device fix bug

--- my-app/hooks/WhatIsDevice/WhatIsDevice.jsx
+++ my-app/hooks/WhatIsDevice/WhatIsDevice.jsx
@@ -3,14 +3,17 @@ import React, {useEffect, useState} from 'react';
 const MOBILE = 'MOBILE';
 const TABLET = 'TABLET';
 const DESKTOP = 'DESKTOP';
+const LARGE = 'LARGE';
 
 const WhatIsDevice = () => {
 
-    const detectedDevice = () => {
+    const detectDevice = () => {
         const queriesToMatchAgainst = {
             mobile: '(max-width: 767px)',
             tablet: '(min-width: 768px) and (max-width: 1023px)',
             desktop: '(min-width: 1024px)',
+            large: '(min-width: 1250)',
+
         };
 
         if (typeof window !== "undefined") {
@@ -21,31 +24,35 @@ const WhatIsDevice = () => {
             if (window.matchMedia(queriesToMatchAgainst.tablet).matches) {
                 return TABLET;
             }
-            return DESKTOP;        }
+            if (window.matchMedia(queriesToMatchAgainst.desktop).matches) {
+                return DESKTOP;
+            }
+            return LARGE;
+        }
     }
-    const [deviceType, setDeviceType] = useState(()=>detectedDevice())
+    const [deviceType, setDeviceType] = useState(() => detectDevice())
 
 
+    useEffect(() => {
+        const resizeHandler = () => {
+            const detectedDevice = detectDevice();
+            if (detectedDevice !== deviceType) {
+                setDeviceType(detectedDevice);
+            }
+        };
+        window.addEventListener('resize', resizeHandler);
+        return () => window.removeEventListener('resize', resizeHandler);
+    }, [deviceType])
 
-
- useEffect(() => {
-     const resizeHandler = () => {
-         const detectedDevice = detectDevice();
-         if (detectedDevice !== deviceType) {
-             setDeviceType(detectedDevice);
-         }
-     };
-     window.addEventListener('resize', resizeHandler);
-     return () => window.removeEventListener('resize', resizeHandler);
- }, [deviceType]);
-
-
-
-    return (
-        <div>
-            <button onClick={() => (console.log(deviceType))}>ffff</button>
-        </div>
-    );
+    return deviceType;
 };
 
 export default WhatIsDevice;
+
+export const DEVICE_TYPES = {
+    MOBILE,
+    TABLET,
+    DESKTOP,
+    LARGE,
+};
+
--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -2,8 +2,12 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Grid from "../components/Grid/Grid";
 import WhatIsDevice from '../hooks/WhatIsDevice/WhatIsDevice'
+import whatIsDevice from "../hooks/WhatIsDevice/WhatIsDevice";
 
 const Home: NextPage = () => {
+
+
+
   return (
    <Grid>
       <Head>
@@ -12,7 +16,7 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-       <WhatIsDevice/>
+
 
    </Grid>
   )
