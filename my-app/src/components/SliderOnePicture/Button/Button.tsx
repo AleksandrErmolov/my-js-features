6c62b2afbf19518ecdd8f3d680cb75493e61249e
Aleksandr Ermolov <AYErmolov@sberbank.ru>
2022-03-30 22:48:50 +0300

    fix error

--- my-app/src/components/SliderOnePicture/Button/Button.tsx
+++ my-app/src/components/SliderOnePicture/Button/Button.tsx
@@ -1,43 +1,37 @@
 import React, {useEffect, useState} from 'react';
-import right from './right.png'
-import left from './left.png'
-import styled from 'styled-components/macro';
-import { css } from 'styled-components';
+import left from './right.png'
+import right from './left.png'
+import styled from 'styled-components';
 
 
 interface ButtonProps {
     direction?: string | undefined;
-    action?: Function | undefined;
+    action?: Function | undefined | string | void | any;
 }
 
 const SliderButton = styled.button`
+  padding: 0;
+  margin: 0;
   border: 0;
   border-radius: 50%;
-  width: 50px;
-  height: 50px;
+  width: 40px;
+  height: 40px;
   background-color: transparent;
+  z-index: 100;
+  display: block;
 `
 
 const Img = styled.img`
-  width: 50px;
-  height: 50px;
+  z-index: 1;
+  width: 40px;
+  height: 40px;
 `
 
 const Button = ({direction, action}: ButtonProps) => {
 
-    const [picture, setPicture] = useState(right)
-
-    useEffect(() => {
-        if (direction === 'left') {
-            setPicture(left)
-        } else {
-            setPicture(right)
-        }
-    },[])
-
     return (
-        <SliderButton>
-            <Img src={picture.src}/>
+        <SliderButton onClick={action}>
+            <Img src={direction ==='left' ? left.src : right.src}/>
         </SliderButton>
     );
 };
--- my-app/src/components/SliderOnePicture/SliderOnePicture.tsx
+++ my-app/src/components/SliderOnePicture/SliderOnePicture.tsx
@@ -1,45 +1,73 @@
-import React, {FC} from 'react';
+import React, {FC, useState} from 'react';
 import {data} from './data/data';
 import styled from 'styled-components';
 import Button from "./Button/Button";
+import img1 from './data/img/img1.jpg'
+import img2 from './data/img/img2.jpg'
+import img3 from './data/img/img3.jpg'
+import img4 from './data/img/img4.jpg'
+import img5 from './data/img/img5.jpg'
+import img6 from './data/img/img1.jpg'
+
 
 const SliderContainer = styled.div`
-  width: 700px;
+  width: 100vh;
   height: 500px;
   position: relative;
-`
-const SliderList = styled.div`
-  width: 100%;
-  height: 100%;
+  z-index: 1;
 `
 
 const Img = styled.img`
+  z-index: 1;
   width: 100%;
   height: 100%;
   object-fit: cover;
   position: absolute;
 `
 
+const ButtonContainer = styled.div`
+  display: flex;
+  width: 700px;
+  position: absolute;
+  bottom: 48%;
+  justifyContent: space-between;
+`
+
 export const SliderOnePicture: FC = () => {
 
+    const [slideIndex, setSlideIndex] = useState(1)
+
     const nextCard = (index: number, e: MouseEvent): void => {
-        console.log(1234)
+        if (slideIndex !== data.length) {
+            setSlideIndex(slideIndex + 1)
+        } else {
+            setSlideIndex(1)
+        }
     }
 
     const prevCard = (index: number, e: MouseEvent): void => {
-        console.log(123)
+        if (slideIndex > 1) {
+            setSlideIndex(slideIndex - 1)
+        } else {
+            setSlideIndex(5)
+        }
     }
 
     return (
+        <div style={{position: 'relative'}}>
             <SliderContainer>
                 {data.map((el, i) =>
-                    <SliderList key={el.id}>
-                        <Img src={el.img.src}/>
-                    </SliderList>
+                    <div key={i} style={{zIndex: 1}}>
+                        <Img style={{backgroundImage: `url("./data/img/img${slideIndex}")`}}/>
+                    </div>
                 )}
-                <Button direction={'left'} action={prevCard}/>
-                <Button direction={'right'} action={nextCard}/>
             </SliderContainer>
+
+            <ButtonContainer>
+                <Button direction='left' action={prevCard}/>
+                <Button direction='right' action={nextCard}/>
+            </ButtonContainer>
+        </div>
     );
 };
 
